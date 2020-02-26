9ed84bb2a6a8e96ba40be4c77bcbc5b1aa6988e0
mwunderlich <martin@wunderlich.com>
2020-02-26 08:19:28 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/jackson/SfgMsscJacksonExampleApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMsscJacksonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMsscJacksonExampleApplication.class, args);
+	}
+
+}
